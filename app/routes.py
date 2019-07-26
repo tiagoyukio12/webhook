4886e2a82acd5b2bd82c15446088b6bd25a1943f
Tiago Fujii <tiagoyukio12@gmail.com>
2019-07-26 13:57:00 -0300

    Integrate with modules in TCC-2019

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,64 @@
 import json
+import calendar
+
+import matplotlib.pyplot as plt
+from app.TCC2019 import info_vis
 
 from app import app
-from flask import request, jsonify
+from flask import request, jsonify, send_file
 
 
 @app.route('/', methods=['POST'])
 def post():
     data = json.loads(request.data)
-    date = data['queryResult']['parameters']['date']
+    
+    if data['queryResult']['parameters']['date'] != '':
+        # Parameter received is a specific date
+        start_date = format_date(data['queryResult']['parameters']['date'])
+        end_date = start_date + 1  # TODO: increment string by 1 day
+    else:
+        # Parameter received is a period
+        start_date = format_date(data['queryResult']['parameters']['date-period']['startDate'])
+        print(start_date)
+        end_date = format_date(data['queryResult']['parameters']['date-period']['endDate'])
 
-    cons = 8
+    cons = info_vis.qry_cons_aggr(start_date, end_date, 'M')
 
-    #return jsonify(fulfillmentText='Você consumiu {} kWh em {}'.format(cons, date))
+    fulfillmentText = 'Você consumiu {} kWh de {} a {}'.format(cons.energy.iloc[0], start_date, end_date)
+
+    # Plot query results
+    fig, ax = plt.subplots()
+    month_names = cons.t.dt.month.apply(lambda x: calendar.month_abbr[x])  # convert month numbers to names
+    plt.bar(month_names, cons.energy, width=0.9, color='lightgreen')
+    plt.ylabel('Consumo [kWh]')
+    ax.set_yticklabels([])  # hide tick labels
+    ax.tick_params(axis=u'both', which=u'both',length=0)  # hide tick marks
+    plt.margins(x=0)  # remove white space
+
+    # Add values at the top of each bar
+    rects = ax.patches
+    bar_labels = ["{:5.0f}".format(cons.iloc[i].energy) for i in range(len(rects))]
+    for rect, label in zip(rects, bar_labels):
+        height = rect.get_height()
+        ax.text(rect.get_x() - 0.05 + rect.get_width()/2, height - 30, label,
+                ha='center', va='bottom', weight = 'bold')
+
+    fig.set_size_inches(12, 6)
+    plt.savefig('./app/plot' + data['responseId'] + '.png')
+
     file_handler = open('./app/response.json', 'r')
     response = json.loads(file_handler.read())
+    response['fulfillmentText'] = fulfillmentText
+
+    url = request.url_root + 'media?file=' + 'plot' + data['responseId'] + '.png'
+    response['fulfillmentMessages'][0]['image']['imageUri'] = url
+    
     return jsonify(response)
+
+def format_date(date):
+    return date[:10] + ' ' + date[11:19]
+
+@app.route('/media', methods=['GET'])
+def get_image():
+    file = request.args.get('file')
+    return send_file(file, mimetype='image/gif')
